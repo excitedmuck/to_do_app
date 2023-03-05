11dfa9745b9e6169a94acbdae98cde965968e14c
excitedmuck <yashvinishukla@gmail.com>
2023-03-05 01:24:17 +0000

    Added configuration files

--- Home.py
+++ Home.py
@@ -18,7 +18,7 @@ for index, todo in enumerate(todos):
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
-        del st.session_state[todo]
+        del st.session_state[index]
         st.experimental_rerun()
 
 st.text_input(label = "Enter a todo:", placeholder= "Add a new todo...", 
